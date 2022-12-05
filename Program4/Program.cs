980a5aeb02c1ae93d361c70da06890b045854738
Roman Filimonov <romatruelove@yandex.ru>
2022-12-05 15:34:06 +0300

    final code

---
+++ Program4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+    if(num1 > num2 && num1 > num3)
+    {
+        Console.WriteLine($"{num1} максимальное число");
+    }
+    else if(num2 > num1 && num2 > num3)
+    {
+        Console.WriteLine($"{num2} максимальное число");
+    }
+    else if(num3 > num1 && num3 > num2)
+    {
+        Console.WriteLine($"{num3} максимальное число");
+    }
---
+++ Program8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+    
+for (int num = 1; num <= N; num++)
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num} ");
+    }
