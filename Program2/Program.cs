94abcb08841feb2b7aec3cd4c517efeaec3f4a99
Roman Filimonov <romatruelove@yandex.ru>
2022-12-05 15:26:57 +0300

    new code

---
+++ Program2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("a = " +num1);
+
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b = " +num2);
+
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1} максимальное число");
+}
+else 
+{
+    Console.WriteLine($"{num2} максимальное число");
+}
